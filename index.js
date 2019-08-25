f3f4683b134ec8d6c821d309928f5de261f3acc4
John Odom <john.osborne.odom@gmail.com>
2019-08-25 13:54:59 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+//let gifts = ["1st gift", "2nd gift", "3rd gift"]
+let newArray = []
+function writeCards(gifts) {
+    for(let i=0; i<gifts.length; i++){
+        newArray[i] = ("Thank you, " + gifts[i] + ", for the wonderful surprise gift!")
+    }
+    return newArray
+}
+
+function countdown(number) {
+    while(number>=0) {
+        console.log(number--)
+    }
+}
