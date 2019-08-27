a36e4e4e7de46cf5a253ba04e8e8b348faa2c913
jonathanboss17 <jtboss47@gmail.com>
2019-08-27 13:39:10 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+import { write } from "fs";
+
 // Code your solutions in this file
+
+function writeCards(names, event) {
+    let array = []; 
+    for (let i = 0; i < names.length; i++) {
+        array.push('Thank you, ' + names[i] + ', for the wonderful ' + event + ' gift!');
+    }
+    return array; 
+}
+
+
+function countdown(end) {
+    while(end > -1)
+        console.log(end--);
+    end
+}
